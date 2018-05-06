e2888fca4097d69de134b4dab992d97f3db5c6cc
Moore.Huang <moore@moorehy.com>
2018-05-06 20:21:12 +0800

    O: 重构expect装饰器的实现，并增加说明文档描述

--- mohand_plugin_expect/hand.py
+++ mohand_plugin_expect/hand.py
@@ -1,10 +1,25 @@
 from mohand.hands import hand
 
-# print('HandDict@mohand_plugin_expect:', hand)
-# print('HandDict@mohand_plugin_expect:', id(hand))
-
 
 def expect(*dargs, **dkwargs):
+    """
+    将被装饰函数封装为一个 :class:`click.core.Command` 类，成为 ``mohand`` 的子命令
+
+    该装饰器可能被作为一个简单无参数的装饰器使用（如： ``@hand.expect`` ）
+    或者包含定制其行为的含参数装饰器使用（如： ``@hand.expect(timeout=30)`` ）
+
+    .. note::
+
+        该装饰器最终会通过插件系统被注册到 :data:`.hands.hand` 中。
+
+        此处的 ``expect`` 装饰器本身是应该不支持无参数装饰的，但考虑到其作为样例实现，
+        故将其实现为兼容两种传参的装饰器
+
+    """
+    invoked = bool(len(dargs) == 1 and not dkwargs and callable(dargs[0]))
+    if invoked:
+        func = dargs[0]
+
     def wrapper(func):
         @hand._click.command(
             name=func.__name__.lower(),
@@ -25,4 +40,4 @@ def expect(*dargs, **dkwargs):
             kwargs.pop('timeout', None)
             return func(*args, **kwargs)
         return _wrapper
-    return wrapper
+    return wrapper if not invoked else wrapper(func)
