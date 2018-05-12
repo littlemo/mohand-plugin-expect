4a3ad4bac6d61a376adf71aeb4ce56d50250f84b
Moore.Huang <moore@moorehy.com>
2018-05-12 21:20:03 +0800

    O: 完善action的expect参数描述文档，支持列表输入以及多种对象

--- mohand_plugin_expect/hand.py
+++ mohand_plugin_expect/hand.py
@@ -107,7 +107,8 @@ class Child(object):
         执行一个action，当检测到符合指定 ``before`` 的 ``expect`` 后，
         发送 ``sendline``
 
-        :param str expect: 期望的输入匹配行
+        :param str expect: 期望的输入匹配行，支持多对象的列表，以及正则表达式对象
+            如：['good', 'bad', re.compile(r'\d'), pexpect.EOF, pexpect.TIMEOUT]
         :param sendline: 发送一行字串，若为正则表达式，则发送匹配到的结果字串
         :type sendline: str or regex
         :param str before: 可选，用来辅助判定期望的输入匹配行，默认为 ``''``
