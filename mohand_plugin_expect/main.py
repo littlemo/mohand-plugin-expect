3cffde74e67ae3cafeab7c21bb62f0a9980dbfb1
Moore.Huang <moore@moorehy.com>
2018-05-13 10:24:05 +0800

    F: 增加对HandBase的version接口支持，返回包名和版本号

--- mohand_plugin_expect/main.py
+++ mohand_plugin_expect/main.py
@@ -1,8 +1,11 @@
 from mohand import hands
 from mohand_plugin_expect.hand import expect
+from mohand_plugin_expect.version import get_cli_version
 
 
 class ExpectHand(hands.HandBase):
     def register(self):
-        # print('ExpectHand:', expect)
         return expect
+
+    def version(self):
+        return 'mohand-plugin-expect', get_cli_version()
