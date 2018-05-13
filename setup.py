d79606f696e0ad8d108c54175e79e1a42f7a7fed
Moore.Huang <moore@moorehy.com>
2018-05-13 15:42:08 +0800

    M: 根据重构后的源码路径，修改setup中的packages设置

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-from mohand_plugin_expect.version import get_setup_version
+from source.mohand_plugin_expect.version import get_setup_version
 
 
 setup(
@@ -14,7 +14,8 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     keywords='mohand plugin automation',
-    packages=find_packages(exclude=('docs', 'tests*')),
+    packages=find_packages('source'),
+    package_dir={'': 'source'},
     include_package_data=True,
     zip_safe=False,
     license='GPLv3',
